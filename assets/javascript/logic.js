f658d8b1bead4b39cae9e3e4b76ac8d84b29d059
Hoo Man <hoomanfor@gmail.com>
2019-09-27 13:45:32 -0400

    rendering gifs onto DOM & generating new gifs if same vibe is clicked

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,13 +2,22 @@
 var vibes = ["Positive", "Grinning", "Beaming", "Joy", "Winking", "Smiling", "Star-Struck", "Zany", "Squinting", "Hugging", "Shushing", "Thinking", "Neutral", "Expressionless", "Smirking", "Unamused", "Relieved", "Pensive", "Sleepy", "Shy", "Partying", "Nerdy", "Confused", "Worried", "Astonished", "Flushed", "Pleading", "Fearful", "Tired", "Evil", "Interested", "Hopeful", "Kindness", "Cheerfulness", "Confidence", "Admiration", "Enthusiasm", "Euphoria", "Satisfaction", "Pride", "Contentment", "Inspired"];
 
 var key = "1hpEc8I1eklOKDKC885JqonMveeNVSS3";
+var offset = 0;
+priorVibe = "";
 
 function displayGifs(query) {
     $.ajax({
         url: query,
         method: "GET"
     }).then(function(response) {
-        console.log(response)
+        var gifsArr = response.data;
+        console.log(gifsArr);
+        gifsArr.forEach(function(element){
+            var gifURL = element.images.fixed_width_small.url;
+            var gifImg = $("<img>");
+            gifImg.attr("src", gifURL);
+            $("#gifs").append(gifImg);
+        })
     })
 }
 
@@ -19,8 +28,15 @@ vibes.forEach(function(element) {
 })
 
 $(document).on("click", "button[type|='button']", function(event) {
+    $("#gifs").empty();
     var vibeText = $(this).text();
-    var query = "https://api.giphy.com/v1/gifs/search?q=" + vibeText + "&limit=10&api_key=" + key;
+        if (priorVibe === vibeText) {
+            offset += 10;
+        } else {
+            offset = 0;
+            priorVibe = vibeText;
+        }
+    var query = "https://api.giphy.com/v1/gifs/search?q=" + vibeText + "&limit=10&offset=" + offset + "&api_key=" + key;
     displayGifs(query)
 })
 
