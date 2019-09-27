cc9a1df4b74689ca91645f85b379abd539a17582
Hoo Man <hoomanfor@gmail.com>
2019-09-27 12:43:29 -0400

    logging response data from GIPHY Search Enpoint

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,8 +1,27 @@
 
-var vibes = ["Positive", "Grinning", "Beaming", "Joy", "Winking", "Kissing", "Smiling", "Star-Struck", "Zany", "Squinting", "Hugging", "Shushing", "Thinking", "Neutral", "Expressionless", "Smirking", "Unamused", "Relieved", "Pensive", "Sleepy", "Nauseated", "Hot", "Cold", "Woozy", "Partying", "Nerdy", "Confused", "Worried", "Astonished", "Flushed", "Pleading", "Fearful", "Tired", "Evil", "Interested", "Hopeful", "Kindness", "Cheerfulness", "Confidence", "Admiration", "Enthusiasm", "Euphoria", "Satisfaction", "Pride", "Contentment", "Inspired"];
+var vibes = ["Positive", "Grinning", "Beaming", "Joy", "Winking", "Smiling", "Star-Struck", "Zany", "Squinting", "Hugging", "Shushing", "Thinking", "Neutral", "Expressionless", "Smirking", "Unamused", "Relieved", "Pensive", "Sleepy", "Shy", "Partying", "Nerdy", "Confused", "Worried", "Astonished", "Flushed", "Pleading", "Fearful", "Tired", "Evil", "Interested", "Hopeful", "Kindness", "Cheerfulness", "Confidence", "Admiration", "Enthusiasm", "Euphoria", "Satisfaction", "Pride", "Contentment", "Inspired"];
+
+var key = "1hpEc8I1eklOKDKC885JqonMveeNVSS3";
+
+function displayGifs(query) {
+    $.ajax({
+        url: query,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response)
+    })
+}
 
 vibes.forEach(function(element) {
     var vibeBtn = $("<button type='button' class='btn btn-primary m-1'>");
     vibeBtn.text(element);
     $("#vibes").append(vibeBtn);
 })
+
+$(document).on("click", "button[type|='button']", function(event) {
+    var vibeText = $(this).text();
+    var query = "https://api.giphy.com/v1/gifs/search?q=" + vibeText + "&limit=10&api_key=" + key;
+    displayGifs(query)
+})
+
+
